21a311438287f41d9480e5fe555463f02d730595
zermiftah <muhammadmiftahudin113@gmail.com>
2022-04-04 06:37:25 +0630

    Adding a 'Header' component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import React, { Fragment } from "react";
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Fragment>
+      <Header></Header>
+    </Fragment>
   );
 }
 
